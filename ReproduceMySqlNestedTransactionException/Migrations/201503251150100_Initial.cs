4c8953a3d8433b4533812b225eff2b0b018a142d
AronVanAmmers <aron@certinio.com>
2015-03-25 14:07:42 +0100

    Initial version. Error reproducible by calling POST api/Authors with the same data 3 times.

---
+++ ReproduceMySqlNestedTransactionException/Migrations/201503251150100_Initial.cs
@@ -0,0 +1,28 @@
+namespace ReproduceMySqlNestedTransactionException.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 128, storeType: "nvarchar"),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Authors", new[] { "Name" });
+            DropTable("dbo.Authors");
+        }
+    }
+}
---
+++ ReproduceMySqlNestedTransactionException/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReproduceMySqlNestedTransactionException.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        [Index(IsUnique=true)]
+        [MaxLength(128)]
+        public string Name { get; set; }
+    }
+}
